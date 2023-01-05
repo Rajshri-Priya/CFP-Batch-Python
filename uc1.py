e0704e26e7df8f6088c48a4600677675f42641af
rajshri-priya <rajshri.priya1708@.gmail.com>
2023-01-05 11:18:34 +0530

    [Rajshri Priya] Added Create Contact[remove contact[] and make it global]

--- uc1.py
+++ uc1.py
@@ -1,26 +1,28 @@
 contacts = []
 
+
 # uc1
 def get_contact_info():
-        return {
-                'first_name': input('Enter the first name: '),
-                'last_name': input('Enter the last name: '),
-                'address': input('Enter the address: '),
-                'city': input('Enter the city: '),
-                'state': input('Enter the state: '),
-                'zip': input('Enter the zip code: '),
-                'phone': input('Enter the phone number: '),
-                'email': input('Enter the email address: ')
-            }
-    
+    return {
+        'first_name': input('Enter the first name: '),
+        'last_name': input('Enter the last name: '),
+        'address': input('Enter the address: '),
+        'city': input('Enter the city: '),
+        'state': input('Enter the state: '),
+        'zip': input('Enter the zip code: '),
+        'phone': input('Enter the phone number: '),
+        'email': input('Enter the email address: ')
+    }
+
 
 def main():
     # AddressBook= []
-    contacts = []
+
     flag = True
     while flag:
         print("\n***WELCOME TO ADDRESS BOOK***\n")
-        print("Enter what you want to do: \n1. Create Contact and Add Contact  \n2. Edit  \n3. Delete \n4. Add multiple contacts \n5. Add multiple AddressBook \n6. Exit")
+        print(
+            "Enter what you want to do: \n1. Create Contact and Add Contact  \n2. Edit  \n3. Delete \n4. Add multiple contacts \n5. Add multiple AddressBook \n6. Exit")
         option = int(input("Enter option: "))
         if option == 1:
             contact_info = get_contact_info()
