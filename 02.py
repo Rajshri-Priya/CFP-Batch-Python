a6749afbaefdb1b789152b8edebdd2365c22a5eb
rajshri-priya <rajshri.priya1708@.gmail.com>
2023-01-10 09:25:24 +0530

    [Rajshri Priya] Added is_even() by lambda(Anonymous fxn)

---
+++ 02.py
@@ -0,0 +1,8 @@
+# by normal way
+def is_even(x):
+    return x % 2 == 0
+
+# A lambda function to check if a number is even:
+is_even = lambda x: x % 2 == 0
+print(is_even(5))  
+print(is_even(6))  
