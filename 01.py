cff54774e321bb09ec14271e9512bc25651feb41
rajshri-priya <rajshri.priya1708@.gmail.com>
2023-01-10 09:24:46 +0530

    [Rajshri Priya] Added add fxn by lambda(Anonymous fxn)

---
+++ 01.py
@@ -0,0 +1,7 @@
+# define a function that takes in two arguments and returns their sum
+def add(x, y):
+    return x + y
+
+# create a lambda function that does the same thing
+add_lambda = lambda x, y: x + y
+print(add_lambda(5, 10)) 
